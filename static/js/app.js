7fbbce7a82e4c8785bbb30e253e049d3c95a7468
conoroshea1996 <conoshea12@gmail.com>
2019-07-29 14:24:35 +0100

    update jquery cdn plus add animation to recipeinfo.html

---
+++ static/js/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('#drop').click(function () {
+        //             ----^----- f should be small letter
+        $('#ingredients-list').slideToggle('slow');
+    });
+});
