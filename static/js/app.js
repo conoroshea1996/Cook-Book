b9ebf2bc8041c97f71cb244c7bc8d39d4bd4712c
conoroshea1996 <conoshea12@gmail.com>
2019-07-29 19:53:22 +0100

    update add recipe to include ingedients and instructions

--- static/js/app.js
+++ static/js/app.js
@@ -3,4 +3,8 @@ $(document).ready(function () {
         //             ----^----- f should be small letter
         $('#ingredients-list').slideToggle('slow');
     });
+    $('#dropit').click(function () {
+        //             ----^----- f should be small letter
+        $('#instuctions-list').slideToggle('slow');
+    });
 });
